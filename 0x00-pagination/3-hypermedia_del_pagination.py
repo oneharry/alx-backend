18d6f3743b4c7c6ce16a40f7f01bdd170f8e0c32
oneharry <harryofficialchidi@yahoo.com>
2023-07-24 21:21:20 +0000

    task 3 implementation

---
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+"""Script for pagination"""
+from typing import Tuple, List
+import csv
+import math
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """ implementation of pagination"""
+    start = (page - 1) * page_size
+    end = start + page_size
+    return start, end
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        """ Initialization method """
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Implement get page"""
+        assert type(page) == int and type(page_size) == int
+        assert page > 0 and page_size > 0
+
+        with open(self.DATA_FILE, 'r') as csvfile:
+            reader = csv.reader(csvfile)
+            data = list(reader)
+        start, end = index_range(page, page_size)
+
+        if start >= len(data):
+            return []
+
+        return data[start:end]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, any]:
+        """ implements get_hyper"""
+        assert type(page) == int and type(page_size) == int
+        assert page > 0 and page_size > 0
+        data_page = self.get_page(page, page_size)
+        total_rows = len(self.dataset())
+        total_pages = math.ceil(len(data_page) / page_size)
+        next_page = page + 1 if page < total_pages else None
+        prev_page = page - 1 if page > 1 else None
+
+        return {
+            'page_size': len(data_page),
+            'page': page,
+            'data': data_page,
+            'next_page': next_page,
+            'prev_page': prev_page,
+            'total_pages': int(total_pages)
+        }
+
+    def get_hyper_index(index: Optional[int] = None, page_size:
+                        int = 10) -> Dict[str, any]:
+        """ implemententation of get hyper"""
+        if index is None:
+            index = 0
+        else:
+            assert isinstance(index, int) and index >= 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        data = self.dataset()
+        total_rows = len(data)
+        next_index = index + page_size
+
+        if next_index >= total_rows:
+            next_index = None
+
+        data_page = data[index:next_index]
+
+        return {
+            'index': index,
+            'next_index': next_index,
+            'page_size': len(data_page),
+            'data': data_page
+        }
+
