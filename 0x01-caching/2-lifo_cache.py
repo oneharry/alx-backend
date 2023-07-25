6879ee0635606f61a45e8cba2285f41983d2ef76
oneharry <harryofficialchidi@yahoo.com>
2023-07-25 10:11:17 +0000

    implemented lifo caching system

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""LIFOCache Module"""
+from collections import deque
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """ LIFOCache implementation """
+
+    def __init__(self):
+        """Initialize """
+        super().__init__()
+        self.queue = deque()
+
+    def put(self, key, item):
+        """ put item value into key key"""
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+        self.queue.append(key)
+        num_cachedata = len(self.cache_data)
+        if num_cachedata > BaseCaching.MAX_ITEMS:
+            rm_key = self.queue.pop()
+            self.cache_data.pop(rm_key)
+            print("DISCARD: {}".format(rm_key))
+
+    def get(self, key):
+        """ Return the value of key in cache"""
+        if key is None or self.cache_data.get(key) is None:
+            return None
+        return self.cache_data.get(key)
