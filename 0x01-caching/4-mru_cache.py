6f6360eaf2c2625902c35578834a3577bafba529
oneharry <harryofficialchidi@yahoo.com>
2023-07-26 09:44:18 +0000

    implemented MRU

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""MRUCache Module"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class  MRUCache(BaseCaching):
+    """ MRUCache implementation """
+
+    def __init__(self):
+        """Initialize """
+        super().__init__()
+        self.queue = []
+
+    def put(self, key, item):
+        """ put item value into key key"""
+        if key is None or item is None:
+            return
+        if key in self.cache_data:
+            self.cache_data.pop(key)
+            self.queue.remove(key)
+        else:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                rm_key = self.queue.pop(-1)
+                self.cache_data.pop(rm_key)
+                print("DISCARD: {}".format(rm_key))
+
+        self.cache_data[key] = item
+        self.queue.append(key)
+
+    def get(self, key):
+        """ Return the value of key in cache"""
+        if key is None or self.cache_data.get(key) is None:
+            return None
+        return self.cache_data.get(key)
